de1d33b59af26af2b2c5228f3773097aea3bc934
Paulo Canedo <paulocanedo@gmail.com>
2018-02-14 17:34:17 -0300

    refactory

--- src/js/ez.js
+++ src/js/ez.js
@@ -1,16 +1,29 @@
 // UI events -------------------------------------------------------------------
-(() => {
-  const menu_link_connection    = document.querySelector('#menu_link_connection');
-  const menu_link_subscriptions = document.querySelector('#menu_link_subscriptions');
-  const menu_link_messages      = document.querySelector('#menu_link_messages');
-  const menu_link_publish       = document.querySelector('#menu_link_publish');
+var ui_utils = (() => {
+  let $ = query => document.querySelector(query);
 
-  const card_connection    = document.querySelector('#card_connection');
-  const card_subscriptions = document.querySelector('#card_subscriptions');
-  const card_messages      = document.querySelector('#card_messages');
+  const menu_link_connection    = $('#menu_link_connection');
+  const menu_link_subscriptions = $('#menu_link_subscriptions');
+  const menu_link_messages      = $('#menu_link_messages');
+  const menu_link_publish       = $('#menu_link_publish');
 
-  const connect_button = document.querySelector('#connect_button');
-  const connection_indicator = document.querySelector('#connection_indicator');
+  const card_connection    = $('#card_connection');
+  const card_subscriptions = $('#card_subscriptions');
+  const card_messages      = $('#card_messages');
+
+  const connect_button = $('#connect_button');
+  const connection_indicator = $('#connection_indicator');
+
+  const connection_form = $('#connection_form');
+  const host_field = connection_form.host;
+  const protocol_field = connection_form.protocol;
+  const port_field = connection_form.port;
+  const clientId_field = connection_form.client_id;
+  const username_field = connection_form.username;
+  const password_field = connection_form.password;
+  const keepAlive_field = connection_form.keep_alive;
+  const ssl_field = connection_form.ssl;
+  const cleanSession_field = connection_form.clean_session;
 
   card_connection.link_card    = menu_link_connection;
   card_subscriptions.link_card = menu_link_subscriptions;
@@ -20,55 +33,79 @@
   const menus = [menu_link_connection, menu_link_subscriptions, menu_link_messages, menu_link_publish];
   let current_card = null;
 
-  var connect_action = () => {
-    var source = connect_button; //this should come from event
-    source.classList.add('is-loading');
-    mqtt_adapter.connect(source);
-  };
+  window.addEventListener('DOMContentLoaded', evt => {
+    // initEvents();
+    var connect_action = () => {
+      var source = connect_button; //this should come from event
+      source.classList.add('is-loading');
+      mqtt_adapter.connect(source);
+    };
 
-  var removeAllCards = () => {
-    for(let card of cards) {
-      card.link_card.classList.remove('is-active');
-      card.style.display = 'none';
+    var removeAllCards = () => {
+      for(let card of cards) {
+        card.link_card.classList.remove('is-active');
+        card.style.display = 'none';
+      }
+    };
+
+    var setCurrentCard = (card) => {
+      removeAllCards();
+
+      current_card = card;
+      card.link_card.classList.add('is-active');
+      card.style.display = 'inherit';
+    }
+
+    var linkCardMenu = (menu, card) => {
+      menu.addEventListener('click', evt => setCurrentCard(card));
+    };
+
+    linkCardMenu(menu_link_connection, card_connection);
+    linkCardMenu(menu_link_subscriptions, card_subscriptions);
+    linkCardMenu(menu_link_messages, card_messages);
+    setCurrentCard(card_connection);
+
+    connect_button.addEventListener('click', connect_action);
+  });
+
+  return {
+    getConnectionParams() {
+      let host = "localhost", protocol = "mqtt", port = 1883, clientId = "",
+          username = "", password = "", keepAlive = 60,
+          ssl = false, cleanSession = true;
+
+      host = host_field.value;
+      protocol = protocol_field.value;
+      port = parseInt(port_field.value);
+      clientId = clientId_field.value;
+      username = username_field.value;
+      password = password_field.value;
+      keepAlive = parseInt(keepAlive_field.value);
+      ssl = ssl_field.value;
+      cleanSession = cleanSession_field.value;
+
+      return {
+        'host': host,
+        'protocol': protocol,
+        'port': port,
+        'clientId': clientId,
+        'username': username,
+        'password': password,
+        'keepAlive': keepAlive,
+        'ssl': ssl,
+        'cleanSession': cleanSession
+      }
     }
   };
-
-  var setCurrentCard = (card) => {
-    removeAllCards();
-
-    current_card = card;
-    card.link_card.classList.add('is-active');
-    card.style.display = 'inherit';
-  }
-
-  var linkCardMenu = (menu, card) => {
-    menu.addEventListener('click', evt => setCurrentCard(card));
-  };
-
-  linkCardMenu(menu_link_connection, card_connection);
-  linkCardMenu(menu_link_subscriptions, card_subscriptions);
-  linkCardMenu(menu_link_messages, card_messages);
-  setCurrentCard(card_connection);
-
-  connect_button.addEventListener('click', connect_action);
-
-  // menu_link_connection.addEventListener('click', evt => setCurrentCard(card_connection));
-  // menu_link_subscriptions.addEventListener('click', evt => setCurrentCard(card_subscriptions));
-  // menu_link_messages.addEventListener('click', evt => setCurrentCard(card_messages));
-
-  // var initEvents = () => {
-  // };
-
-  // window.addEventListener('load', evt => {
-    // initEvents();
-  // });
 })();
 // end UI events ---------------------------------------------------------------
 
 const mqtt_adapter = (() => {
   return {
     connect(source) {
-      var client = mqtt.connect('ws://localhost', {connectTimeout: 3 * 1000}, () => {});
+      let params = ui_utils.getConnectionParams();
+
+      var client = mqtt.connect(`${params.protocol}://${params.host}`, {connectTimeout: 3 * 1000}, () => {});
 
       client.on('connect', connack => {
         source.classList.remove('is-loading');
