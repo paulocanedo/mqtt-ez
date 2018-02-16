ddf5b36baf4d52a3101404df154df0b881103797
Paulo Canedo <paulocanedo@gmail.com>
2018-02-16 18:09:59 -0300

    subscribe and message incoming working

--- src/js/ez.js
+++ src/js/ez.js
@@ -1,4 +1,110 @@
-// UI events -------------------------------------------------------------------
+let table_util = (() => {
+  let createColumnColor = (args = {}) => {
+    let column = document.createElement('td');
+    let color_link = document.createElement('a');
+    let color = document.createElement('i');
+
+    color_link.title = "Choose a color";
+    color.classList.add('fas', 'fa-square', 'fa-lg');
+
+    color_link.appendChild(color);
+    column.appendChild(color_link);
+
+    return {
+      setColor(color) {
+      },
+      root() {
+        return column;
+      }
+    };
+  };
+  let createColumnText = (args = {}) => {
+    let column = document.createElement('td');
+    let textNode = document.createTextNode(args.text || '');
+    column.className = args.alignment || '';
+    column.appendChild(textNode);
+
+    return {
+      setText(text) {
+        textNode.textContent = text;
+      },
+      setAlignment(alignment) {
+        column.className = alignment;
+      },
+      root() {
+        return column;
+      }
+    };
+  };
+
+  let createColumnControls = (args = {}) => {
+    let column = document.createElement('td');
+    column.className = 'has-text-right';
+
+    let createButton = () => {
+      let a = document.createElement('a');
+      a.href = '#';
+      a.className = 'button';
+
+      return a;
+    };
+    let createIcon = (icon_code) => {
+      let icon = document.createElement('i');
+      icon.classList.add('fas', icon_code);
+
+      return icon;
+    };
+    let wrapIcon = (icon) => {
+      let wrapper = document.createElement('span');
+      wrapper.className = 'icon';
+      wrapper.appendChild(icon);
+      return wrapper;
+    };
+
+    let editButton = createButton();
+    let editIcon = createIcon('fa-edit');
+
+    let deleteButton = createButton();
+    let deleteIcon = createIcon('fa-trash');
+
+    editButton.appendChild(wrapIcon(editIcon));
+    deleteButton.appendChild(wrapIcon(deleteIcon));
+
+    column.appendChild(editButton);
+    column.appendChild(document.createTextNode(' '));
+    column.appendChild(deleteButton);
+
+    return {
+      setAlignment(alignment) {
+        column.className = alignment;
+      },
+      root() {
+        return column;
+      }
+    };
+  };
+  return {
+    COLUMN_COLOR: 0,
+    COLUMN_TEXT: 1,
+    COLUMN_CONTROLS: 2,
+    createColumn(type, args) {
+      switch (type) {
+        case table_util.COLUMN_COLOR:
+          return createColumnColor(args);
+          break;
+        case table_util.COLUMN_TEXT:
+          return createColumnText(args);
+          break;
+        case table_util.COLUMN_CONTROLS:
+          return createColumnControls(args);
+          break;
+        default:
+
+      }
+    }
+  };
+})();
+
 var ui_utils = (() => {
   let $ = query => document.querySelector(query);
 
@@ -28,6 +134,9 @@ var ui_utils = (() => {
   const subscriptions_form = $('#subscriptions_form');
   const subscribe_button = subscriptions_form.subscribe_button;
 
+  const topics_table = $('#topics_table');
+  const messages_table = $('#messages_table');
+
   card_connection.link_card    = menu_link_connection;
   card_subscriptions.link_card = menu_link_subscriptions;
   card_messages.link_card      = menu_link_messages;
@@ -46,18 +155,60 @@ var ui_utils = (() => {
       mqtt_adapter.on('connect', connack => {
         source.classList.remove('is-loading');
         connection_indicator.classList.replace('has-text-grey-light', 'has-text-success');
+        menu_link_connection.style.backgroundColor = '#23d160';
       });
 
       mqtt_adapter.on('error', error => {
         source.classList.remove('is-loading');
         connection_indicator.classList.replace('has-text-grey-light', 'has-text-danger');
+        menu_link_connection.style.backgroundColor = '#ff0000';
       });
     };
 
     var subscribe_action = () => {
+      let newLine = (color, topic, qos) => {
+        let cColor = table_util.createColumn(table_util.COLUMN_COLOR);
+        let cTopic = table_util.createColumn(table_util.COLUMN_TEXT, {text: topic});
+        let cQoS = table_util.createColumn(table_util.COLUMN_TEXT, {text: qos, alignment: 'has-text-centered'});
+        let cControls = table_util.createColumn(table_util.COLUMN_CONTROLS);
+
+        let line = document.createElement('tr');
+        for(let column of [cColor, cTopic, cQoS, cControls])
+          line.appendChild(column.root());
+
+        return line;
+      };
+
+      let randomColor = () => {
+        return '#0000ff';
+      };
+
+      let color = randomColor();
       let topic = subscriptions_form.topic.value;
+      let qos = subscriptions_form.qos.value;
+      let line = newLine(color, topic, qos);
+
+      topics_table.tBodies[0].appendChild(line);
 
       mqtt_adapter.subscribe(topic);
+      mqtt_adapter.on('message', (topic, message) => {
+        let newLine = (color, topic, message, qos, retained) => {
+          let cColor = table_util.createColumn(table_util.COLUMN_COLOR);
+          let cTopic = table_util.createColumn(table_util.COLUMN_TEXT, {text: topic});
+          let cMessage = table_util.createColumn(table_util.COLUMN_TEXT, {text: message});
+          let cQoS = table_util.createColumn(table_util.COLUMN_TEXT, {text: `${qos}`, alignment: 'has-text-centered'});
+          let cRetained = table_util.createColumn(table_util.COLUMN_TEXT, {text: retained, alignment: 'has-text-right'});
+
+          let line = document.createElement('tr');
+          for(let column of [cColor, cTopic, cMessage, cQoS, cRetained])
+            line.appendChild(column.root());
+
+          return line;
+        };
+
+        let line = newLine('#0000ff', topic, message, '0', 'No');
+        messages_table.tBodies[0].appendChild(line);
+      });
     };
 
     var removeAllCards = () => {
@@ -82,6 +233,7 @@ var ui_utils = (() => {
     linkCardMenu(menu_link_connection, card_connection);
     linkCardMenu(menu_link_subscriptions, card_subscriptions);
     linkCardMenu(menu_link_messages, card_messages);
+    // setCurrentCard(card_subscriptions);
     setCurrentCard(card_connection);
 
     connect_button.addEventListener('click', connect_action);
@@ -145,7 +297,7 @@ const mqtt_adapter = (() => {
       return {
         'host': 'localhost',
         'protocol': 'ws',
-        'port': 1883,
+        'port': 80,
         'clientId': clientId,
         'keepAlive': 60,
         'ssl': false,
@@ -154,6 +306,7 @@ const mqtt_adapter = (() => {
     },
     connect(params) {
       let url = params.protocol + (params.ssl ? 's' : '') + '://' + params.host;
+      console.log(url);
 
       client = mqtt.connect(`${url}`, {
         'keepalive': params.keepAlive,
